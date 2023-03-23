b4fdaae6c9f15e64b8aa7dcceb234d7053ebeb0c
Henry Zelenak <zele0397@fredonia.edu>
2023-03-23 10:30:22 -0400

    Update pair_sum with two linear solutions

--- pair_sum/pair_sum.cpp
+++ pair_sum/pair_sum.cpp
@@ -7,20 +7,52 @@ bool hasPairWithSum(const std::vector<int>&, int, int mode = 0);
 int main() {
 	std::vector<int> nums = { 1,3,5,7,8,8,9,12,13 };
 	int sum = 12;
-	std::cout << "The result is " << hasPairWithSum(nums, sum, 0) << std::endl;
+	std::cout << "The result for quadratic is " << hasPairWithSum(nums, sum, 0) << std::endl;
+	std::cout << "The result for linear is " << hasPairWithSum(nums, sum, 1) << std::endl;
+	std::cout << "The result for linear2 is " << hasPairWithSum(nums, sum, 2) << std::endl;
+
 
 	return 0;
 }
 
 bool hasPairWithSum(const std::vector<int>& nums, int sum, int mode) {
-	for (int i = 0; i < nums.size(); i++) {
-		for (int j = 0; j < nums.size(); j++) {
-			if (i != j) {
-				if ((nums[i] + nums[j]) == sum) {
-					std::cout << "Position " << i << " (" << nums[i] << ") has a pair with position " << j << " (" << nums[j] << ")" << std::endl;
-					return true;
+	if (mode == 0) {
+		for (int i = 0; i < nums.size(); i++) {
+			for (int j = 0; j < nums.size(); j++) {
+				if (i != j) {
+					if ((nums[i] + nums[j]) == sum) {
+						std::cout << "Position " << i << " (" << nums[i] << ") has a pair with position " << j << " (" << nums[j] << ")" << std::endl;
+						return true;
+					}
 				}
 			}
 		}
 	}
+	else if (mode == 1) {
+		int start = 0;
+		int end = nums.size() - 1;
+
+		while (start < end) {
+			if (nums[start] + nums[end] == sum) {
+				std::cout << "\n" << start << "-" << end;
+				return true;
+			}
+			else if (nums[start] + nums[end] > sum) {
+				end--;
+			}
+			else start++;
+		}
+	}
+	else if (mode == 2) {
+		std::unordered_set<int> compliments;
+
+		for (int x : nums) {
+			if (compliments.find(x) != compliments.end()) {
+				return true;
+			}
+			compliments.insert(sum - x);
+		}
+	}
+	return false;
 }
+
