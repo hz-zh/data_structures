9e87fd0c496493cd4f4af89bde2cbcb93b1cec71
Henry Zelenak <zele0397@fredonia.edu>
2023-04-11 10:51:29 -0400

    Add getInOrderSucc and contains and part of remove methods

--- BST-wo-classes/main.cpp
+++ BST-wo-classes/main.cpp
@@ -4,6 +4,8 @@
 bool isEmpty(BinaryNode<int>*);
 void printTree(BinaryNode<int>*);
 BinaryNode<int>* add(BinaryNode<int>* rt, int newEntry);
+bool contains(BinaryNode<int>*, int);
+void printLeaves(BinaryNode<int>*);
 
 int main() {
 	BinaryNode<int>* root = nullptr;
@@ -14,8 +16,10 @@ int main() {
 	root = add(root, 5);
 	root = add(root, 20);
 
+	std::cout << contains(root, 10) << std::endl;
 
-	printTree(root);
+	printLeaves(root);
+	//printTree(root);
 }
 
 bool isEmpty(BinaryNode<int>* rt) {
@@ -63,5 +67,74 @@ BinaryNode<int>* add(BinaryNode<int>* rt, int newEntry) {
 			}
 		}
 		return rt;
+	}
+}
+
+bool contains(BinaryNode<int>* rt, int anEntry) {
+	BinaryNode<int>* cur = rt;
+	if (!isEmpty(cur)) {
+		if (cur->getValue() == anEntry) { return true; }
+		contains(cur->getLeftChildPtr(), anEntry);
+		contains(cur->getRightChildPtr(), anEntry);
+		return false;
+	}
+	else return false;
+}
+
+void printLeaves(BinaryNode<int>* rt) {
+	/*if (!isEmpty(rt)) {
+		if (rt->getLeftChildPtr() == nullptr && rt->getRightChildPtr() == nullptr) {
+			std::cout << rt->getValue() << std::endl;
+		}
+		printLeaves(rt->getLeftChildPtr());
+		printLeaves(rt->getRightChildPtr());
+	}*/
+	if (!isEmpty(rt)) {
+		if (rt->getRightChildPtr() != nullptr) {
+			if (rt->getLeftChildPtr() == nullptr && rt->getRightChildPtr() == nullptr) {
+				std::cout << " Right side leaf: " << rt->getValue() << std::endl;
+			}
+			printLeaves(rt->getRightChildPtr());
+		}
+		else if (rt->getLeftChildPtr() != nullptr) {
+			if (rt->getLeftChildPtr() == nullptr && rt->getRightChildPtr() == nullptr) {
+				std::cout << " Left side leaf: " << rt->getValue() << std::endl;
+			}
+			printLeaves(rt->getLeftChildPtr());
+		}
+		else {
+			printLeaves(rt->getLeftChildPtr());
+			printLeaves(rt->getRightChildPtr());
+		}
+	}
+}
+
+BinaryNode<int>* getInOrderSuccessor(BinaryNode<int>* cur) {
+	std::cout << "Inside getInOrderSuccessor() starting -->" << cur->getValue() << std::endl;
+	while (cur->getLeftChildPtr() != nullptr) {
+		std::cout << cur->getValue() << " ";
+		cur = cur->getLeftChildPtr();
+	}
+	std::cout << std::endl << "successor: " << cur->getValue() << endl;
+	return cur;
+}
+
+BinaryNode<int>* remove(BinaryNode<int>* cur, int removeValue) {
+	std::cout << "\nAt: " << cur->getValue() << " ";
+	//BinaryNode<int>* cur = root REMOVED
+	if (!isEmpty(cur)) {
+		if (contains(cur, removeValue)) {
+			// start recursive calls
+			// if removeValue < cur 
+			if (removeValue < cur->getValue()) {
+				std::cout << "\n" << removeValue << " is less than " << cur->getValue() << std::endl;
+				cur->setLeftChildPtr(remove(cur->getLeftChildPtr(), removeValue));
+			}
+			// if removeValue > cur 
+			else if (removeValue > cur->getValue()) {
+				std::cout << "\n" << removeValue << " is greater than " << cur->getValue() << endl;
+				cur->setRightChildPtr(remove(cur->getRightChildPtr(), removeValue));
+			}
+		}
 	}
 }
